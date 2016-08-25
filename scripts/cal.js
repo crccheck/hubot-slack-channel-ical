0f9290d1cc0a0df391e53e3e4609196fec902d6b
crccheck <c@crccheck.com>
2016-08-25 05:04:50 +0000

    Add http entrypoint

--- scripts/cal.js
+++ scripts/cal.js
@@ -3,18 +3,33 @@
 
 // https://github.com/github/hubot/blob/master/docs/scripting.md#http-listener
 
+
 module.exports = (robot) => {
-  robot.hear(/./, (msg) => {
+  robot.router.get('/hubot/calendar/:room.ical', (req, res) => {
     const client = robot.adapter.client
-    client.web.pins
-    const channel = msg.message.room
-    client.web.pins.list(channel, (err, hmm) => {
+    const room = req.params.room
+    const channel = client.rtm.dataStore.getChannelByName(room)
+    // console.log('channel', channel)
+
+    if (!channel) {
+      // 404
+      // TODO early exit because we can only deal with channels that exist
+    }
+
+    if (!channel.has_pins) {
+      // 400 ?
+      // TODO early exit because we can only deal with channels with pinned items
+    }
+
+    client.web.pins.list(channel.id, (err, data) => {
       if (err) {
         // TODO handler error
+      } else {
+        data.items.forEach((x) => {
+          console.log(x)
+        })
       }
-      hmm.items.forEach((x) => {
-        console.log(x)
-      })
+      res.send('TODO')
     })
   })
 }
